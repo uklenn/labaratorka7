1fd1a79b02b9b0854d93229f9182ae179d0b49d5
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 19:00:52 +0300

    Delete unt-3.py

--- unt-3.py
+++
@@ -1,7 +0,0 @@
-# Факториал числа
-def factorial():
-    n = int(input("Введите целое неотрицательное число: "))
-    factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
-    print("Факториал числа", n, "равен", factorial)
