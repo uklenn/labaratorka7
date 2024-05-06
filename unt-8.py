938f18b0baecddf02fdaea415686c7a8320de2a1
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 19:02:32 +0300

    Delete unt-8.py

--- unt-8.py
+++
@@ -1,5 +0,0 @@
-# Сумма элементов списка
-def sum_of_list():
-    numbers = [int(x) for x in input("Введите числа через пробел: ").split()]
-    total = sum(numbers)
-    print("Сумма элементов списка:", total)
