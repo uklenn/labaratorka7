64b7fdd9ae5e6fb023f0ba40963942b003683957
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 19:02:00 +0300

    Delete unt-7.py

--- unt-7.py
+++
@@ -1,9 +0,0 @@
-# Подсчет гласных и согласных
-def count_vowels_consonants():
-    string = input("Введите строку: ").lower()
-    vowels = 'aeiouаеёиоуыэюя'
-    consonants = 'bcdfghjklmnpqrstvwxyzбвгджзйклмнпрстфхцчшщ'
-    vowel_count = sum(1 for char in string if char in vowels)
-    consonant_count = sum(1 for char in string if char in consonants)
-    print("Количество гласных:", vowel_count)
-    print("Количество согласных:", consonant_count)
