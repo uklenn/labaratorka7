51e46383b5252901860ecb6176da5553c81124d8
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 19:00:35 +0300

    Delete unt-2.py

--- unt-2.py
+++
@@ -1,5 +0,0 @@
-# Поиск максимума
-def find_max():
-    numbers = [int(input("Введите число " + str(i+1) + ": ")) for i in range(3)]
-    max_number = max(numbers)
-    print("Максимальное число:", max_number)
