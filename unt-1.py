803851b194a89579755312fc813cc610ec5c8cd5
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 18:59:56 +0300

    Delete unt-1.py

--- unt-1.py
+++
@@ -1,7 +0,0 @@
-# Проверка на четность/нечетность
-def check_even_odd():
-    number = int(input("Введите целое число: "))
-    if number % 2 == 0:
-        print("Число", number, "четное.")
-    else:
-        print("Число", number, "нечетное.")
