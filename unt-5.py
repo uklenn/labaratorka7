68f703a3fb3c8741ced8f92533664a1c3f53b60b
uklenn <157277078+uklenn@users.noreply.github.com>
2024-05-06 19:01:35 +0300

    Delete unt-5.py

--- unt-5.py
+++
@@ -1,6 +0,0 @@
-# Таблица умножения
-def multiplication_table():
-    number = int(input("Введите число для таблицы умножения: "))
-    print("Таблица умножения для", number)
-    for i in range(1, 11):
-        print(number, "x", i, "=", number * i)
